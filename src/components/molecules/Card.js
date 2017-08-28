57e3e65887569efbd35b1015c607f1ea8c0849af
Vitor Amaral <va.works@hotmail.com>
2017-08-28 19:32:15 +0000

    Simple correction to the Cards UI

--- src/components/molecules/Card.js
+++ src/components/molecules/Card.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 //Components
 import { Title, SubTitle, Icon } from "../atoms/index";
-import { Textfit } from "react-textfit";
 //Styles
 import Colors from "../../styles/Colors";
 import styled from "styled-components";
