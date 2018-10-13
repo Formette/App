fcad37e9474be074e2b0ba821d6b8bcb7e854c1c
Vitor Amaral <va.works@hotmail.com>
2018-10-13 19:03:11 +0100

    removed big test on footer

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -40,14 +40,6 @@ const Footer = props => {
             Feedback
           </Link>
         </li>
-        <li>
-          <Link
-            href={`https://formette.freshdesk.com/support/home`}
-            target="_blank"
-          >
-            Big Test
-          </Link>
-        </li>
       </ul>
     </div>
   );
