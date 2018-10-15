21941e0ed22a6d10040a424a2dcb89b5a6b23885
Vitor Amaral <va.works@hotmail.com>
2018-10-15 12:14:18 +0100

    fixe eslint

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -89,6 +89,7 @@ module.exports = {
     return string.charAt(0).toUpperCase() + string.slice(1);
   },
   _validateEmail(email) {
+    // eslint-disable-next-line
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   },
